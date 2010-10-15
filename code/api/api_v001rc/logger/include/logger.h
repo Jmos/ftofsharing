858913fb676143f5a516d300d9c9ec80e8b5b134
MuellnerMarco <MuellnerMarco@ed7a1941-e62f-a9da-ef21-68f2f9546975>
2010-10-15 08:31:02 +0000

    [feature] wiki entry for xmlDatalib! [Internal] first implementations of loggerclass..

---
+++ code/api/api_v001rc/logger/include/logger.h
@@ -0,0 +1,36 @@
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+/**************************************************************************************
+ *
+ * api_v001rc   -   2010
+ *
+ * File logger.h , 15.10.2010, created by muma
+ *
+ **************************************************************************************/
+
+// Qt Includes
+
+// Std Includes
+
+// Linux Includes
+
+// Windows Includes
+
+// Library Includes
+
+// Application Includes
+
+// Defines
+
+/**************************************************************************************/
+
+class logger
+    {
+
+    public:
+	logger();
+	~logger();
+    private:
+}
+
+#endif //__LOGGER_H__
---
+++ code/api/api_v001rc/logger/include/loggerlib.h
@@ -0,0 +1,29 @@
+#ifndef __LOGGERLIB_H__
+#define __LOGGERLIB_H__
+/**************************************************************************************
+ *
+ * API_v001rc   -   2010
+ *
+ * File loggerlib.h , 15.10.2010, created by muma
+ *
+ **************************************************************************************/
+
+// Qt Includes
+
+// Std Includes
+
+// Linux Includes
+
+// Windows Includes
+
+// Library Includes
+
+// Application Includes
+
+// Defines
+
+/**************************************************************************************/
+
+#define LOG_NO_ENTRY logger __lets_log()
+
+#endif //__LOGGERLIB_H__
