951793eaab0b253927586e69d09e9ea225f88c25
MuellnerMarco <MuellnerMarco@ed7a1941-e62f-a9da-ef21-68f2f9546975>
2010-10-15 11:59:36 +0000

    [feature] implemented save method (at the omment not buildable!)

--- code/api/api_v001rc/xmlDataLib/include/xmldatalib.h
+++ code/api/api_v001rc/xmlDataLib/include/xmldatalib.h
@@ -87,6 +87,63 @@ public:
 	memcpy(pTargetData, readData.constData(), sizeof(pTargetData));
 	return true;
 	}
+
+    bool writeData(void *pGottenData, QString Filename, QString Name)
+	{
+	if (Filename.isEmpty() || Name.isEmpty())
+	    return false; //FIXME LOGGING!
+
+	QString fileAndPath;
+	fileAndPath.append(Filename);
+	QFile xmlData(fileAndPath);
+
+	QDomDocument objectDoc("objectType");
+	QString errMsg;
+	QByteArray readData;
+
+	if (!objectDoc.setContent(&xmlData, &errMsg))
+	    {
+	    xmlData.close();
+	    printf("***************ERROR****************************************************\n");
+	    printf("***************Path to file: <%s>***************************************\n",qPrintable(fileAndPath));
+	    printf("***************couldn't set content for DOM!. Errmsg: <%s>**************",qPrintable(errMsg));
+	    return false;
+	    }
+
+	if(!objectDoc.hasChildNodes())
+	    {
+	    printf("***************ERROR****************************************************\n");
+	    printf("***************objectdoc has no Child nodes*****************************\n");
+	    return false;
+	    }
+
+	QDomNodeList nodes = objectDoc.elementsByTagName(Name);
+
+	    QDomNode n = nodes.item(0);
+	    if(!n.hasChildNodes())
+		{
+		printf("***************ERROR*************************************************************\n");
+		printf("***************Node:<%s> has no Child nodes**************************************\n");
+		}
+	    QDomNodeList subNodes = n.childNodes();
+
+	    if(!subNodes.item(0).isElement())
+		{
+		printf("***************ERROR*************************************************************\n");
+		printf("***************Node is no element!***********************************************\n");
+		return false;
+		}
+
+	    QDomElement e = subNodes.item(0).toElement();
+	    if(e.hasAttribute("value"))
+		{
+		memcpy(readData, pGottenData, sizeof(pGottenData));
+
+		QString value((char*)pGottenData,sizeof(pGottenData));
+		e.setAttribute("value",value);
+		}
+	return true;
+	}
 private:
     };
 
@@ -98,8 +155,9 @@ public:
 	{
 	initData();
 	XmlRead getData;
+	readObjName = objName;
 
-	if (!getData.readData(&m_data, "objects.xml", objName))//fixme xmlfile irgendwie dynamisch übergeben. Konstruktor?
+	if (!getData.readData(&m_data, "objects.xml", readObjName))//fixme xmlfile irgendwie dynamisch übergeben. Konstruktor?
 	    printf("shitz happenedz!\n"); //FIXME loggerklasse einbauen
 	}
 
@@ -112,6 +170,13 @@ public:
 	return m_data;
 	}
 
+    XmlDataLib<T>& operator=(const XmlDataLib<T>& other)
+      {
+          m_data = other.m_data;
+          writeToDb();
+          return *this;
+      };
+
 private:
 
     void initData()
@@ -119,7 +184,16 @@ private:
 	memset(&m_data, 0, sizeof(T));
 	}
 
+    void writeToDb()
+	{
+	XmlRead writeData;
+
+	if(!writeData.writeData(&m_data,"objects.xml",readObjName))
+	    printf("shitz happenedz!\n"); //FIXME Loggerklasse einbauen
+	}
+
     T m_data; ///< die Daten von \a m_plName
+    QString readObjName;
     };
 
 //Application Includes
